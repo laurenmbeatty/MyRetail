c5cb3ac24b43ea9e75851d6e2b77462f3850d21b
Lauren Beatty <lmbeatty@umn.edu>
2018-12-27 19:45:08 -0600

    get data wired up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React from "react";
 import { render } from "react-dom";
-
+import productData from "./item-data";
 
 class App extends React.Component {
+  state = { loading: true, productData };
+
+  componentDidMount() {
+    this.setState({
+      loading: false
+    });
+  }
   render() {
-    return (
-      <div>
-        <h1>Hello!</h1>
+    const { productData, loading } = this.state;
+    return !loading ? (
+      <div className="main">
+        <div className="col-1">
+          <h1>
+            <pre>{productData}</pre>
+          </h1>
+        </div>
+        <div className="col-2">
+          <h2>Column 2</h2>
+        </div>
       </div>
-    );
+    ) : null;
   }
 }
 render(<App />, document.getElementById("root"));
